6d19c1367c608576d3820513453c297b195016df
Celine Paulin <celpau01@gmail.com>
2024-03-19 14:29:23 +0100

    3e commit les toast ont marche je les ai commentes j'en suis a la question 3c

--- app/src/main/java/fr/isen/paulin/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/paulin/androiderestaurant/CategoryActivity.kt
@@ -1,20 +1,39 @@
 package fr.isen.paulin.androiderestaurant
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import fr.isen.paulin.androiderestaurant.ui.theme.AndroidERestaurantTheme
 
 class CategoryActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_category)
+        val category = intent.getStringExtra("category") ?: ""
+
+        val fakeDishes = when(category){
+            "Entrées" -> resources.getStringArray(R.array.fake_starters)
+            "Plats" -> resources.getStringArray(R.array.fake_dish)
+            "Desserts" -> resources.getStringArray(R.array.fake_desert)
+            else -> resources.getStringArray(R.array.fake_dish)
+        }
+        // Utilisez la catégorie pour afficher les plats correspondants
         setContent {
             AndroidERestaurantTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +41,37 @@ class CategoryActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting2("Android")
+                    CategoryComponent(category, fakeDishes.asList()){ dish ->
+                        onDishClicked(dish)
+                    }
                 }
             }
         }
     }
+    private fun onDishClicked(dish: String){
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra("dish",dish)
+        startActivity(intent)
+    }
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    AndroidERestaurantTheme {
-        Greeting2("Android")
+fun CategoryComponent(category: String, dishes: List<String>, onDishClicked: (String) -> Unit) {
+    Column {
+        Text(
+            text = "Hello $category!",
+            textAlign = TextAlign.Center,
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(32.dp)
+                .fillMaxWidth()
+        )
+    }
+    LazyColumn (Modifier.padding(24.dp)){
+        items(dishes) {dish ->
+            Text(text = dish,
+                Modifier.padding(0.dp, 8.dp)
+                    .clickable { onDishClicked(dish) })
+        }
     }
 }
--- app/src/main/java/fr/isen/paulin/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/paulin/androiderestaurant/HomeActivity.kt
@@ -1,5 +1,6 @@
 package fr.isen.paulin.androiderestaurant
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -34,55 +35,74 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidERestaurantTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeComponent { message ->
+                        navigateToCategory(message)
+                    }
                 }
             }
         }
     }
+    private fun navigateToCategory(category: String) {
+        val intent = Intent(this@MainActivity, CategoryActivity::class.java).apply {
+            putExtra("category", category)
+        }
+        startActivity(intent)
+    }
+
+    /*fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }*/
+
 }
 
 
-
 @Composable
-fun Greeting( modifier: Modifier = Modifier) {
+fun HomeComponent(navigateToCategory: (String) -> Unit) {
+
     Column {
         Row {
-            Column (
-                modifier = modifier.padding(16.dp), // Ajout de padding
+            Column(
+                modifier = Modifier.padding(16.dp), // Ajout de padding
                 verticalArrangement = Arrangement.Top, // Espacement vertical
                 horizontalAlignment = Alignment.Start // Alignement horizontal au début
-            ){
+            ) {
                 Text(
                     text = "Bienvenue ",
                     textAlign = TextAlign.Center,
                     fontSize = 24.sp,
-                    color = Color(0xFFFF8000) ,
-                    modifier = modifier
+                    color = Color(0xFFFF8000),
+                    modifier = Modifier
                 )
                 Text(
                     text = "chez ",
                     textAlign = TextAlign.Center,
                     fontSize = 24.sp,
-                    color = Color(0xFFFF8000) ,
-                    modifier = modifier
+                    color = Color(0xFFFF8000),
+                    modifier = Modifier
                 )
                 Text(
                     text = "DroidRestaurant ",
                     textAlign = TextAlign.Center,
                     fontSize = 24.sp,
                     //fontStyle = FontStyle.,
-                    color = Color(0xFFFF8000) ,
-                    modifier = modifier
+                    color = Color(0xFFFF8000),
+                    modifier = Modifier
                 )
             }
-            Image(painter = painterResource(id = R.drawable.android_cuisine), contentDescription="")
+            Image(
+                painter = painterResource(id = R.drawable.android_cuisine),
+                contentDescription = ""
+            )
         }
         // Ajout d'espace entre le message et les boutons
         Spacer(modifier = Modifier.height(32.dp))
         // Boutons
         Column(
-            modifier = modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth(),
             verticalArrangement = Arrangement.Center, // Centrer verticalement
             horizontalAlignment = Alignment.CenterHorizontally // Centrer horizontalement
         ) {
@@ -91,45 +111,53 @@ fun Greeting( modifier: Modifier = Modifier) {
             Text(
                 text = "Entrées",
                 fontSize = 24.sp,
-                color = Color(0xFFFF8000) ,
-                modifier = Modifier.padding(vertical = 4.dp)
-                    .clickable {  /*showToast(context,"Entrées")*/ }// Ajouter un padding vertical pour espacer les textes
+                color = Color(0xFFFF8000),
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .clickable {
+                        navigateToCategory("Entrées")
+
+                    }// Ajouter un padding vertical pour espacer les textes
             )
             Divider(modifier = Modifier.padding(horizontal = 16.dp))
             Text(
                 text = "Plats",
                 color = Color(0xFFFF8000),
                 fontSize = 24.sp,
-                modifier = Modifier.padding(vertical = 4.dp)
-                    .clickable { /*showToast(context,"Plats") */}// Ajouter un padding vertical pour espacer les textes
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .clickable {
+                        navigateToCategory("Plats")
+                        /*coroutineScope.launch {
+                            showToast("Plats")
+                        }*/
+                    }// Ajouter un padding vertical pour espacer les textes
             )
             Divider(modifier = Modifier.padding(horizontal = 16.dp))
             Text(
                 text = "Desserts",
                 color = Color(0xFFFF8000),
                 fontSize = 24.sp,
-                modifier = Modifier.padding(vertical = 4.dp)
-                    .clickable {/* showToast(context,message = "Desserts") */}// Ajouter un padding vertical pour espacer les textes
+                modifier = Modifier
+                    .padding(vertical = 4.dp)
+                    .clickable {
+                        navigateToCategory("Desserts")
+                        /*coroutineScope.launch {
+                            showToast("Desserts")
+                        }*/
+                    }// Ajouter un padding vertical pour espacer les textes
             )
 
         }
     }
-    }
+}
 
 
-//@Composable
-/*fun showToast(context: Context, message: String) {
-    LaunchedEffect(Unit) {
-        //val context = LocalContext.current
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-    }
-}*/
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidERestaurantTheme {
-        Greeting()
-
+        HomeComponent {}
     }
 }
